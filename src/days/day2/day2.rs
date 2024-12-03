5b2caca40cfd67ddc09fb8f4a4d54a34ce7e4359
Payton Harris <payton.harris@goodmaps.com>
2024-12-02 21:21:17 -0500

    Improve performance by tracking failing indexes
    Only try removing the indexes around the failing index.

--- src/days/day2/day2.rs
+++ src/days/day2/day2.rs
@@ -1,4 +1,5 @@
 use crate::{file_reader, helper::string_to_i32, WhichPuzzle};
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 #[cfg(test)]
 mod tests;
@@ -15,6 +16,14 @@ enum Direction {
     Asc,
     Desc,
     Unset,
+}
+
+static CALL_COUNT: AtomicUsize = AtomicUsize::new(0);
+
+// For performance tracking, record how many times a function is called by incrementing
+// this static counter.
+fn increment_call_count() {
+    CALL_COUNT.fetch_add(1, Ordering::SeqCst);
 }
 
 // Boilerplate to decide whether to run part one or two and which input to use.
@@ -39,7 +48,7 @@ fn run_one(input: &str) -> i32 {
     for line in input.lines() {
         let nums: Vec<i32> = convert_line_to_nums_vector(&line);
 
-        if check_if_safe(&nums) {
+        if check_if_safe(&nums).0 {
             safe_count += 1;
         }
     }
@@ -54,14 +63,18 @@ fn run_two(input: &str) -> i32 {
     for line in input.lines() {
         let nums: Vec<i32> = convert_line_to_nums_vector(&line);
 
-        if check_if_safe(&nums) {
+        let (safe, problem_index) = check_if_safe(&nums);
+
+        if safe {
             safe_count += 1;
         } else {
-            if check_if_safe_after_removing_one(&nums) {
+            if check_if_safe_after_removing_one(&nums, problem_index) {
                 safe_count += 1;
             }
         }
     }
+
+    // println!("called {}", CALL_COUNT.load(Ordering::SeqCst));
 
     safe_count
 }
@@ -73,8 +86,10 @@ fn convert_line_to_nums_vector(nums: &str) -> Vec<i32> {
         .collect::<Vec<_>>()
 }
 
-/// Checks the array based on the rules, and returns `true` if safe, `false` if unsafe.
-fn check_if_safe(nums: &Vec<i32>) -> bool {
+/// Checks the array based on the rules, and returns `(true, 0)` if safe, `(false, problem_index)` if unsafe.
+/// The `problem_index` is the index where the safety check did not pass.
+fn check_if_safe(nums: &Vec<i32>) -> (bool, usize) {
+    increment_call_count();
     let mut dir = Direction::Unset;
 
     // Compare each number to the number in front of it.
@@ -83,36 +98,48 @@ fn check_if_safe(nums: &Vec<i32>) -> bool {
 
         // Check if the difference is between 1 and 3.
         if diff.abs() < 1 || diff.abs() > 3 {
-            return false;
+            return (false, i);
         }
 
         // Check if the value is consistently ascending or descending.
         if diff > 0 {
             match dir {
-                Direction::Desc => return false,
+                Direction::Desc => return (false, i),
                 Direction::Unset => dir = Direction::Asc,
                 _ => {}
             }
         } else if diff < 0 {
             match dir {
-                Direction::Asc => return false,
+                Direction::Asc => return (false, i),
                 Direction::Unset => dir = Direction::Desc,
                 _ => {}
             }
         }
     }
 
-    true
+    (true, 0)
 }
 
-/// Naively try removing one item at a time until the sequence is safe,
-/// or give up after trying all possibilities.
-fn check_if_safe_after_removing_one(nums: &Vec<i32>) -> bool {
-    for i in 0..nums.len() {
+/// Remove the indexes before, at, and after the problem_index and return true if
+/// one fixes the problem. Otherwise return false.
+fn check_if_safe_after_removing_one(nums: &Vec<i32>, problem_index: usize) -> bool {
+    // Start at problem_index - 1 unless it's already at 0.
+    let starting_index = if problem_index == 0 {
+        0
+    } else {
+        problem_index - 1
+    };
+
+    for i in starting_index..=problem_index + 1 {
+        if i >= nums.len() {
+            println!("Out of range");
+            continue;
+        }
+
         let mut nums_filtered = nums.clone();
         nums_filtered.remove(i);
 
-        if check_if_safe(&nums_filtered) {
+        if check_if_safe(&nums_filtered).0 {
             return true;
         }
     }
