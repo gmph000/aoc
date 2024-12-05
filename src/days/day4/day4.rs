c511efb8bc1d3d79dcecb73411b7232908927634
Payton Harris <payton.harris@goodmaps.com>
2024-12-04 22:59:39 -0500

    Solve 4.2

--- src/days/day4/day4.rs
+++ src/days/day4/day4.rs
@@ -56,6 +56,8 @@ fn run_one(input: &str) -> i32 {
     matches
 }
 
+// Look in all directions to see if the X is the beginning of an XMAS.
+// Return the number of XMASs that started at this location.
 fn traverse_all_directions(matrix: &Vec<Vec<char>>, i: i32, j: i32) -> i32 {
     let expected = "XMAS".chars().collect::<Vec<_>>();
     let directions = [
@@ -91,7 +93,7 @@ fn traverse_all_directions(matrix: &Vec<Vec<char>>, i: i32, j: i32) -> i32 {
             }
 
             let val = matrix[loc_x][loc_y];
-            // println!("{val} at {loc_x},{loc_y}");
+
             if val != expected[step] {
                 continue 'dirs;
             }
@@ -104,6 +106,70 @@ fn traverse_all_directions(matrix: &Vec<Vec<char>>, i: i32, j: i32) -> i32 {
 }
 
 // Part two solution.
-fn run_two(_input: &str) -> i32 {
-    0
+fn run_two(input: &str) -> i32 {
+    let lines = input.lines().collect::<Vec<_>>();
+
+    let mut matrix: Vec<Vec<char>> = vec![];
+    for line in lines {
+        matrix.push(line.chars().collect::<Vec<_>>());
+    }
+
+    let mut matches = 0;
+
+    for i in 0..matrix.len() {
+        for j in 0..matrix[i].len() {
+            // Find every 'A' which is always the center of an X-MAS.
+            if matrix[i][j] == 'A' {
+                matches += is_x_mas_center(
+                    &matrix,
+                    i32::try_from(i).expect(""),
+                    i32::try_from(j).expect(""),
+                )
+            }
+        }
+    }
+
+    matches
+}
+
+// If you've found an 'A', check if it has an 'S' and 'M' diagonally, but not
+// the same in both line. Return 1 if it's a X-MAS, 0 if not.
+fn is_x_mas_center(matrix: &Vec<Vec<char>>, i: i32, j: i32) -> i32 {
+    let top_left_to_bottom_right = [(-1, 1), (1, -1)];
+    let bottom_left_to_top_right = [(-1, -1), (1, 1)];
+
+    for checks in vec![top_left_to_bottom_right, bottom_left_to_top_right] {
+        let mut found_m = false;
+        let mut found_s = false;
+
+        // Remember j is like x, i is like y.
+        for dir in checks {
+            let loc_x = match usize::try_from(j + dir.0) {
+                Ok(val) => val,
+                Err(_) => return 0,
+            };
+            let loc_y = match usize::try_from(i + dir.1) {
+                Ok(val) => val,
+                Err(_) => return 0,
+            };
+
+            if loc_y >= matrix.len() || loc_x >= matrix[0].len() {
+                return 0;
+            }
+
+            let val = matrix[loc_y][loc_x];
+
+            if val == 'M' {
+                found_m = true;
+            } else if val == 'S' {
+                found_s = true;
+            }
+        }
+
+        if !found_s || !found_m {
+            return 0;
+        }
+    }
+
+    1
 }
--- src/days/day4/tests.rs
+++ src/days/day4/tests.rs
@@ -2,12 +2,12 @@ use super::*;
 
 #[test]
 fn solves_day2_1() {
-    assert_eq!(run(WhichPuzzle::First, true), 0);
-    assert_eq!(run(WhichPuzzle::First, false), 0);
+    assert_eq!(run(WhichPuzzle::First, true), 18);
+    assert_eq!(run(WhichPuzzle::First, false), 2493);
 }
 
 #[test]
 fn solves_day2_2() {
-    assert_eq!(run(WhichPuzzle::Second, true), 0);
-    assert_eq!(run(WhichPuzzle::Second, false), 0);
+    assert_eq!(run(WhichPuzzle::Second, true), 9);
+    assert_eq!(run(WhichPuzzle::Second, false), 1890);
 }
